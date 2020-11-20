Fixes and calculation delay measurement

FrankWolfe.py
@@ -1,6 +1,7 @@
 from scipy.optimize import linprog
 import numdifftools as nd
 import numpy as np
+import time
 
 
 class frank_wolfe():
@@ -18,15 +19,18 @@ class frank_wolfe():
         self.s_t = []
         self.f_t = []
         self.violation = 0
+        self.time = 0
         
     def __repr__(self):
         out = 'f_min: '+str(self.f_min)+'\n' + \
                 'x_min: '+str(self.x_min)+'\n' + \
-                'violation: '+str(self.violation)
+                'violation: '+str(self.violation)+'\n' + \
+                'time: '+str(self.time)
         return out
 
     def optimize(self):
         x = self.x0
+        t1 = time.time()
         for i in range(0,self.iterations):
             gamma = 2 / (i+2)
             grad_def = nd.Gradient(self.min_fun)
@@ -40,7 +44,9 @@ class frank_wolfe():
             x = x + gamma*(s-x)
             self.f_t.append(self.min_fun(x))
             self.x_t.append(x)
-            
+        t2 = time.time()
+        self.time = t2 - t1
+        
         constraints = np.dot(self.A,x) - self.b
         self.violation = np.sum([i for i in constraints if i > 0])
         self.x_min = x

fw_main.py
@@ -10,54 +10,50 @@ def min_fun(x):
     """The function to minimize"""
     return 15*(x[0]-1)**2 + x[1]**2 + 0.5*x[2]**2 + 2*x[0]*x[1] - x[0]*x[2] - 2*x[0] + 6*x[1]
 
-def main():
 
-    # number of constraints and variables
-    constr_num = 4
-    var_num = 3
+# number of constraints and variables
+constr_num = 4
+var_num = 3
 
-    # Ax <= ub
-    A = np.random.randint(-10,10, (constr_num,var_num))
-    ub = 10*np.ones(constr_num)
+# Ax <= ub
+A = np.random.randint(-10,10, (constr_num,var_num))
+ub = 10*np.ones(constr_num)
 
-    # 0 <= x_i <= 10
-    bounds=[(0,10) for i in range(0,var_num)]
-
-    # Initial point x0
-    x0 = np.random.randint(0,10,(var_num))
+# 0 <= x_i <= 10
+bounds=[(0,10) for i in range(0,var_num)]
+# Initial point x0
+x0 = np.random.randint(0,10,(var_num))
 
 
-    # Use Frank-Wolfe
-    iterations = 200
-    fw = frank_wolfe(min_fun,A,ub,bounds,x0,iterations)
-    results = fw.optimize()
+# Use Frank-Wolfe
+iterations = 200
+fw = frank_wolfe(min_fun,A,ub,bounds,x0,iterations)
+results = fw.optimize()
 
 
-    # Use a convex solver
-    bounds = Bounds(np.zeros(var_num), 10*np.ones(var_num))
-    lb = -np.inf*np.ones(constr_num)
-    linear_constraint = LinearConstraint(A, lb, ub)
+# Use a convex solver
+bounds = Bounds(np.zeros(var_num), 10*np.ones(var_num))
+lb = -np.inf*np.ones(constr_num)
+linear_constraint = LinearConstraint(A, lb, ub)
 
-    res = minimize(min_fun, x0, method='trust-constr', jac=nd.Gradient(min_fun),
+res = minimize(min_fun, x0, method='trust-constr', jac=nd.Gradient(min_fun),
                 constraints=linear_constraint, bounds=bounds,
                 options={'verbose': 0,'gtol': 1e-8, 'disp': True})
 
 
-    # Optimality gap
-    f_star = [res.fun for i in range(0,len(results.f_t))]
+# Optimality gap
+f_star = [res.fun for i in range(0,len(results.f_t))]
 
-    fig = plt.figure()
-    mpl.rc('xtick', labelsize=16) 
-    mpl.rc('ytick', labelsize=16) 
-    plt.yscale('log')
-    plt.grid()
-    plt.plot(range(0,iterations), np.abs(np.array(f_star) - np.array(results.f_t)),'bo-',lw=2)
-    plt.ylabel('Optimality Gap',fontsize=20)
-    plt.xlabel('Iteration',fontsize=20)
-    plt.tight_layout()
-    plt.show()
-    fig.savefig('opt_gap.jpeg', format='jpg')
+fig = plt.figure()
+mpl.rc('xtick', labelsize=16) 
+mpl.rc('ytick', labelsize=16) 
+plt.yscale('log')
+plt.grid()
+plt.plot(range(0,iterations), np.abs(np.array(f_star) - np.array(results.f_t)),'bo-',lw=2)
+plt.ylabel('Optimality Gap',fontsize=20)
+plt.xlabel('Iteration',fontsize=20)
+plt.tight_layout()
+plt.show()
+#fig.savefig('opt_gap.jpeg', format='jpg')
     
-    
-if __name__ == '__main__':
-    main()
+